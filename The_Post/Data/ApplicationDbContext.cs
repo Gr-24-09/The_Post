2253a99484ae3670a1f0d054acff142fb549065a
Basel <basel.eibo4676@gmail.com>
2025-01-20 16:20:45 +0100

    Created Subscription Models , Created UserModels , Adding the FirstName , LastName , Dob prop to the register view

--- The_Post/Data/ApplicationDbContext.cs
+++ The_Post/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using The_Post.Models;
 
 namespace The_Post.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Subscription> Subscriptions { get; set; }
     }
 }
