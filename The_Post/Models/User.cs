c629ad5d7bbfcf4dc54e0d3427a0f6d20ec0b180
Basel <basel.eibo4676@gmail.com>
2025-01-24 12:19:57 +0100

    Adding Address , City , Zip to the user Model .

--- The_Post/Models/User.cs
+++ The_Post/Models/User.cs
@@ -19,6 +19,21 @@ namespace The_Post.Models
         [DataType(DataType.Date)]
         public DateTime? DOB { get; set; }
 
+        [Required(ErrorMessage = "Delivery address is required.")]
+        [StringLength(50, ErrorMessage = "Delivery address can't be longer than 50 characters.")]
+        
+        public string Address { get; set; }
+
+        [Required(ErrorMessage = "Delivery City is required.")]
+        [StringLength(50, ErrorMessage = "Delivery city can't be longer than 50 characters.")]
+        
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Delivery Zip is required.")]
+        [RegularExpression(@"^\d{5}$", ErrorMessage = "Delivery zip must be a 5-digit number.")]
+        public string Zip { get; set; }
+
+
         public List<Subscription> Subscriptions { get; set; } = new List<Subscription>();
     }
 }
