5768ddeec63325ab3fae0e887643ba1a3dcc2584
Palunken <cedric.dile@gmail.com>
2025-01-31 12:04:58 +0100

    Deleted the old category code and views

--- The_Post/Controllers/NewsController.cs
+++ The_Post/Controllers/NewsController.cs
@@ -32,42 +32,5 @@ namespace The_Post.Controllers
    
             return View(model);
         }
-
-        //public  IActionResult Local()
-        //{
-        //    var local = _articleService.GetAllArticlesByCategory(1);
-        //    return View(local);
-        //}
-
-        //public IActionResult Sweden()
-        //{
-        //    var sweden = _articleService.GetAllArticlesByCategory(2);
-        //    return View(sweden);
-                
-        //}
-
-        //public IActionResult World()
-        //{
-        //    var world = _articleService.GetAllArticlesByCategory(3);
-        //    return View(world);
-        //}
-
-        //public IActionResult Weather()
-        //{
-        //    var weather = _articleService.GetAllArticlesByCategory(4);
-        //    return View(weather);
-        //}
-
-        //public IActionResult Economy()
-        //{
-        //    var economy = _articleService.GetAllArticlesByCategory(5);
-        //    return View(economy);
-        //}
-
-        //public IActionResult Sports()
-        //{
-        //    var sports = _articleService.GetAllArticlesByCategory(6);
-        //    return View(sports);
-        //}
     }
 }
