6756e6ccff975eb31d757c52dfbbf9c78750dede
Basel <basel.eibo4676@gmail.com>
2025-02-24 17:01:57 +0100

    change the task to task<bool> ,

--- The_Post/Services/IEmployeeService.cs
+++ The_Post/Services/IEmployeeService.cs
@@ -8,7 +8,7 @@ namespace The_Post.Services
     {
         public Task<IdentityResult> AddEmployee(User user, string password);
         public Task AssignRole(string userId, string role);        
-        public Task DeleteEmployee(string userId);
+        public Task<bool> DeleteEmployee(string userId);
         public Task EditEmployee(User user);
         public Task<List<User>> GetAllEmployees();
         public Task<List<EmployeeVM>> GetAllEmployeesWithRolesAsync();
