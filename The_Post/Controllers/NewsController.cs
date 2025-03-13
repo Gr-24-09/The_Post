adbe2d578c31efca70f1f084fc6db5974955bc11
MadhuriSeshu <madhurisai5055@gmail.com>
2025-03-13 12:27:18 +0100

    removed unwanted code

--- The_Post/Controllers/NewsController.cs
+++ The_Post/Controllers/NewsController.cs
@@ -38,9 +38,9 @@ namespace The_Post.Controllers
         public async Task<IActionResult> CategoryView(string categoryName)
         {
             var model = new CategoryPageVM()
-            {
+            { 
                 //Articles = articles,
-                Category = categoryName
+                Category = categoryName               
             };
 
             // Redirects to the weather action if category is weather
@@ -56,10 +56,9 @@ namespace The_Post.Controllers
                 {
                     var date = DateTime.Now.AddDays(-1).ToString("yyyy-MM-dd");
                     var regions = new List<string> { "SE1", "SE2", "SE3", "SE4" };
-                    //var hours = new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23" };
                     var allPrices = new List<TableEntity>();
 
-                    foreach (var region in regions) //iterating through all the hours
+                    foreach (var region in regions) //iterating through all the regions
                     {
                         var prices = await GetHistoricalElectricityPrices(region, date);
                         allPrices.AddRange(prices);
@@ -83,7 +82,7 @@ namespace The_Post.Controllers
 
         private async Task<List<TableEntity>> GetHistoricalElectricityPrices(string region, string date)
         {
-            //var partitionKey = date;
+            
             if (string.IsNullOrEmpty(region) || string.IsNullOrEmpty(date))
             {
                 // Handle the case where region is null or empty
@@ -93,7 +92,6 @@ namespace The_Post.Controllers
             }
             var partitionKey = date;
 
-            //var hourNumber = int.Parse(hour);  //Directly parsing the hour
 
             var regionBase = region.Substring(0, region.Length - 1); // "SE"
             var regionNumber = int.Parse(region.Substring(region.Length - 1)); // "1" -> 1
@@ -123,20 +121,16 @@ namespace The_Post.Controllers
         {
             var model = new HistoricalElectricityPricesViewModel();
             var regions = new List<String> { "SE1", "SE2", "SE3", "SE4" };
-            //var hours = new List<string> { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23" };
-
 
             var allPrices = new List<TableEntity>();
 
             try
             {
-                var queryDate = string.IsNullOrEmpty(date) ? DateTime.UtcNow.AddDays(-1).ToString("yyyy-MM-dd") : date;
-                //var regions = new List<String> { "SE1","SE2","SE3","SE4" };
+                var queryDate = string.IsNullOrEmpty(date) ? DateTime.UtcNow.AddDays(-1).ToString("yyyy-MM-dd") : date; 
 
-                //var allPrices = new List<TableEntity>();
                 if (regions != null && regions.Any())
                 {
-                    foreach (var region in regions)  //we had regions before.Replaced region with hour
+                    foreach (var region in regions)  
                     {
                         var prices = await GetHistoricalElectricityPrices(region, queryDate);
                         allPrices.AddRange(prices); // Add fetched prices to the list
